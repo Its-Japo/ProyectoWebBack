2b9f41b994061f927f87c1ee3a3b175e5bf65adb
Japo <pue22296@uvg.edu.gt>
2024-05-02 21:36:52 -0600

    Cambio de conexión

--- server/src/conn.js
+++ server/src/conn.js
@@ -3,7 +3,7 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  host: 'dpg-coq4dqf79t8c7389muug-a.virginia-postgres.render.com',
+  host: 'dpg-coq4dqf79t8c7389muug-a',
   user: 'blog_user',
   password: 'XkIwg4vTxHCottOpl552aOjySukSq65o',
   database: 'blog_db_pcqz',
